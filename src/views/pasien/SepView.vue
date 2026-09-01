f8e3a29f450ab37f483ecfffe6a63a25ee5da6c4
Ferry Ardiansyah <ferard.diansyah@gmail.com>
2026-09-01 08:52:15 +0700

    fix(sep): add missing getPaginationPages function in SepView.vue

--- src/views/pasien/SepView.vue
+++ src/views/pasien/SepView.vue
@@ -386,6 +386,29 @@ const changePage = (page) => {
   fetchSEP()
 }
 
+const getPaginationPages = () => {
+  const current = pagination.current_page
+  const last = pagination.last_page
+  const pages = []
+
+  if (last <= 7) {
+    for (let i = 1; i <= last; i++) pages.push(i)
+  } else {
+    pages.push(1)
+    if (current > 3) pages.push('...')
+    
+    const start = Math.max(2, current - 1)
+    const end = Math.min(last - 1, current + 1)
+    
+    for (let i = start; i <= end; i++) pages.push(i)
+    
+    if (current < last - 2) pages.push('...')
+    pages.push(last)
+  }
+
+  return pages
+}
+
 const openCekPesertaModal = () => {
   selectedCekPesertaData.noKartu = ''
   selectedCekPesertaData.nik = ''
